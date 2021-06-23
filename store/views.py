c467a70d05b4d955f88bbf4e4a30d5334a25fd7b
IrinaPanov <iwell1202@gmail.com>
2021-06-23 16:45:23 -0400

    permissions added

--- store/views.py
+++ store/views.py
@@ -3,6 +3,7 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 
 from store.models import Book
+from store.permissions import IsOwnerOrStaffOrReadOnly
 from store.serializers import BooksSerializer
 
 
@@ -10,8 +11,12 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    permission_classes = [IsOwnerOrStaffOrReadOnly]
     filter_fields = ['price']
     search_fields = ['name', 'author']
     ordering_fields = ['price', 'author']
 
+    def perform_create(self, serializer):
+        serializer.validated_data['owner'] = self.request.user
+        serializer.save()
 
