9baa57abc3f2eb7b5613e94a2320cbf125297097
IrinaPanov <iwell1202@gmail.com>
2021-06-28 15:10:21 -0400

    annotate

--- store/serializers.py
+++ store/serializers.py
@@ -1,12 +1,17 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from store.models import Book, UserBookRelation
 
 
 class BooksSerializer(ModelSerializer):
+    annotated_likes = serializers.IntegerField()
+
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = ('id', 'name', 'price', 'author', 'annotated_likes')
+
+
 
 
 class UserBookRelationSerializer(ModelSerializer):
